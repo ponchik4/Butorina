c4f0103c2fe4b790326c64b5742513b8fccf6ad3
Butorina <Rysinda4@gmail.com>
2019-03-21 14:48:11 +0300

    и еще немного

--- lesson2/task3.rb
+++ lesson2/task3.rb
@@ -2,7 +2,7 @@ arr = [1, 1]
 next_id = 2
 
 while next_id < 100 do
-  arr.push (next_id)
+  arr.push(next_id)
   next_id = arr.last + arr[-2]
 end
 
--- lesson2/task4.rb
+++ lesson2/task4.rb
@@ -1,1 +1,8 @@
-arr[a...z]
+a = []
+('а'..'я').each {|i| a.push(i)}
+letters = %w[а о и е ё э ы у ю я]
+alph = {}
+a.each.with_index(1) do |value, key|
+  alph[value] = key if letters.include?(value)
+end
+puts alph
