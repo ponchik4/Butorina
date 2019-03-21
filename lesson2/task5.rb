9031d63c27cd767d32cfcdfc93d046d55cc04453
Butorina <Rysinda4@gmail.com>
2019-03-21 21:39:44 +0300

    И еще немного

--- lesson2/task5.rb
+++ lesson2/task5.rb
@@ -2,7 +2,7 @@ puts "Введите день:"
 day = gets.to_i
 
 puts "Введите месяц:"
-month = gets.to_i - 1
+month = gets.to_i
 
 puts "Введите год:"
 year = gets.to_i
@@ -13,5 +13,5 @@ leap_year = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
 months[1] = 29 if leap_year
 
 today = 0
-today = day + months.inject(0, :+)
+today = day + months.take(month - 1).inject(0, :+)
 puts "Сегодня #{today} день"
