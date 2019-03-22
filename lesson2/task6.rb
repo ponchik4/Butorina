9cdf83133836b2c2b3c998ce8b8e7392a9b1c85b
Butorina <Rysinda4@gmail.com>
2019-03-22 19:23:22 +0300

    lesson2

--- lesson2/task6.rb
+++ lesson2/task6.rb
@@ -1,10 +1,21 @@
+check = {}
+check2 = {}
+total = 0
 
 loop do
-  puts "Введите название товара, если товаров больше нет введите 'стоп'"
-  product = gets.chomp
-  break if product == "стоп"
-  puts "Введите цену за единицу"
-  price = gets.to_f
-  puts "ведите кол-во купленного товара"
-  quantity = gets.to_i
-  end
+      puts "Введите название товара, если товаров больше нет введите 'стоп'"
+      product = gets.chomp
+      break if product == "стоп"
+      puts "Введите цену за единицу"
+      price = gets.to_f
+      puts "ведите кол-во купленного товара"
+      quantity = gets.to_f
+
+  check[product] = { price: price, quantity: quantity }
+  check2[product] = price * quantity
+  total = total + check2[product]
+end
+
+puts check
+puts "Итоговая сумма за каждый товар: #{check2}"
+puts "Всего на сумму: #{total}"
