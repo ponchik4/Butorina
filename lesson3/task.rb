2de9b995e2b27ca94e0b0a97bcdca4006cea31da
rysa <rysinda4@gmail.com>
2019-03-29 16:57:20 +0300

    Исправления1

--- lesson3/task.rb
+++ lesson3/task.rb
@@ -1,7 +1,5 @@
 class Station
-
-  attr_accessor :trains
-  attr_reader :name
+  attr_reader :name, :trains
 
   def initialize(name)
    @name = name
@@ -9,63 +7,61 @@ class Station
   end
 
   def add_train (train)
-    self.trains << train
+    @trains << train
   end
 
   def remove_train(train)
-    self.trains.delete(train)
+    @trains.delete(train)
   end
 
   def get_train_by_type(type)
-    self.trains.select.count { |train| train.type == train }
+    @trains.select.count { |train| train.type == train }
   end
-
 end
 
 
-
-
 class Route
-
-  attr_accessor :stations
+  attr_reader :stations
 
   def initialize (first_station, last_station)
-    @stations = [first_station, last_station]
+    @first_station = first_station
+    @last_station = last_station
+    @stations = [@first_station, @last_station]
   end
 
   def add_stations(station)
-    @stations.add(1, station)
+    @stations.push(station)
   end
 
   def intermediate_staion(station)
-    stantions.delete(station)
+    return if [@first_station, @last_station].include?(station)
+    @stantions.delete(station)
   end
 
   def show_staion
     @stantions.each { |station| puts station.name }
   end
-
 end
 
 
-
-Класс Train (Поезд):
-
-
-
-
 class Train
-
   attr_accessor :speed
-  attr_reader :carriage, :add_carriage, :show_staion, :next_station, :current_station, :previous_station
+  attr_reader :carriage, :current_station, :route
 
-
-  def initialize (number, type, carriage, route)
+  def initialize (number, type)
     @number = number
     @type = type
-    @carriage = carriage
-    @route = route.stations[0]
-    @current_station = @route
+    @carriage = 0
+  end
+
+  def increase_speed(speed = 5)
+    @speed += speed
+    @speed = 0 if @speed < 0
+  end
+
+  def decrease_speed(speed = 5)
+    @speed -= speed
+    @speed = 0 if @speed < 0
   end
 
   def stop
@@ -73,23 +69,41 @@ class Train
   end
 
   def add_carriage
-    @carriage.add +=1 if @speed == 0
+    @carriage + 1 if @speed == 0
   end
 
   def delete_carriage
-    @carriage.delete -=1 if @speed == 0
+    @carriage - 1 if @speed == 0
+    @carriage = 0 if @carriage < 0
+  end
+
+  def set_route(route)
+    @route = route
+    @current_station = route.stations.first
+    @current_station_index = 0
   end
 
   def current_station
    @route.stations[@current_station]
   end
 
+  def move_forward
+   @current_station.remove_train(self)
+   @current_station = next_station
+   @current_station.add_train(self)
+  end
+
+  def move_back
+    @current_station.remove_train(self)
+    @current_station = previous_station
+    @current_station.add_train(self)
+  end
+
   def next_station
-   @route.stations[@current_station + 1]
+   @route.stations[@current_station_index + 1]
   end
 
   def previous_station
-   @route.stations[@current_station - 1]
+   @route.stations[@current_station_index - 1] if @current_station_index > 0
   end
-
 end
